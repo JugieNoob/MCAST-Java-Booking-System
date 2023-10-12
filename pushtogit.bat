b5f65ad2f7ec50f3c6f966f676bf76a58dc6533c
JugieNoob <jurgenarna@gmail.com>
2023-10-12 19:46:00 +0200

    test

--- pushtogit.bat
+++ pushtogit.bat
@@ -5,7 +5,7 @@ set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
 git remote set-url --add --push origin https://github.com/JugieNoob/MCAST-Java-Booking-System.git
 git pull
-git push origin main --allow-unrelated-histories
+git push origin main
 echo "Committed to Github!"
 echo "Press ENTER to close this window!"
 set /p EnterToClose=
