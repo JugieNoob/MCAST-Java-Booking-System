3e2ae24c77a0b6ff9717c91cfb803cd4debab753
JugieNoob <jurgenarna@gmail.com>
2023-10-12 19:44:03 +0200

    first fork commit

--- pushtogit.bat
+++ pushtogit.bat
@@ -4,8 +4,8 @@ git add .
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
 git remote set-url --add --push origin https://github.com/JugieNoob/MCAST-Java-Booking-System.git
-git pull origin main --allow-unrelated-histories
-git push origin main
+git pull
+git push origin main --allow-unrelated-histories
 echo "Committed to Github!"
 echo "Press ENTER to close this window!"
 set /p EnterToClose=
